1c946959b10f82cc57ca96794f371cc07beaa14d
David Murphy <117365216@umail.ucc.ie>
2018-02-27 15:58:54 +0000

    Add welcome page

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -8,11 +8,32 @@ session_start();
     </head>
     <body>
         <h4>RECEIPT</h4>
+        
+         <?php  
+        // Set session variables
+          $_SESSION["firstname"] = $_POST["firstname"];
+          $_SESSION["surname"] = $_POST["surname"];
+          $_SESSION["email"] = $_POST["email"];
+        ?>
+        
+        <p>
+            <?php
+            // Echo session variables that were set on the previous pages
+            echo "Name: " . $_SESSION["firstname"] . ["surname"] . ".";
+            ?>
+        </p>
+        <br/>
+        
+        <p>
+            <?php
+            // Echo session variables that were set on the previous pages
+            echo "Email: " . $_SESSION["email"] . ".";
+            ?>
+        </p>
+        <br/>
                 
         <?php
         // Echo session variables that were set on previous page
-        echo "Name:" . $_SESSION["firstname"] . ["surname"] . ".";
-        echo "Email Address" . $_SESSION["email"] . ".";
         echo "Total is " . $_SESSION["total"] . ".";
         ?>
     </body>
