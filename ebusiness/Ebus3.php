8933dc36adf38aee516088fe51a7ed2b19a5a427
David Murphy <117365216@umail.ucc.ie>
2018-03-07 00:32:26 +0000

    restoring files that WIPED

---
+++ ebusiness/Ebus3.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>RECEIPT</title>
+    </head>
+    <body>
+        <center><h4>RECEIPT</h4></center>
+        
+         
+        <center>
+        <p>
+            <?php
+            // Echo session variables that were set on the previous pages
+            echo "Name: " . $_SESSION["firstname"] . " " . $_SESSION["surname"] . ".";
+            ?>
+        </p>
+        </center>
+        <br/>
+        
+        <center>
+        <p>
+            <?php
+            // Echo session variables that were set on the previous pages
+            echo "Email: " . $_SESSION["email"] . ".";
+            ?>
+        </p>
+        </center>
+        <br/>
+        
+        <center>   
+        <?php
+        // Echo session variables that were set on previous page
+        echo "Total is " . $_SESSION["total"] . ".";
+        ?>
+        </center> 
+        
+    </body>
+</html> 
---
+++ ebusiness/ebushome.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+        a:link, a:visited {
+    background-color: #00ff00;
+    color: black;
+    padding: 14px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+}
+
+
+a:hover, a:active {
+    background-color: white;
+}
+        </style>
+         <title>E-Business Homepage</title>
+    </head>
+    <body>
+         <center><h1 class="mainheading" style="color:cyan">Welcome to Cloud Services Shop!</h1></center>
+         
+         <center><p style="font-size:20px;">As explained in my interests pages, I have a keen interest in Cloud services. Thus, 
+         I have decided to set up an E Business in which I will supply the four products: Salesforce, 
+         Cloud 9, Amazon Web Services and GMail. The products are available at unbeatable prices, thus, 
+         in my opinion, this is the one stop shop for all of your cloud service needs. Please feel free 
+         to explore our products and prices on the next page.</p></center>
+         <br/>
+         
+         <center><img src="https://vault.ecloud.co.uk/ukfast-blog/uploads/2013/10/ecloud.jpg" style="width:300px;height:250px;"></center>
+         <br/>
+         
+         <center>
+             <a href="Ebus1.php">Products and Rates!</a>
+             <a href="../homepage.html">Home</a>
+         </center>
+    </body>
+</html> 
